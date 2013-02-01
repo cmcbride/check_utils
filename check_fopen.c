57d6ec2114b338d158974f25ce9c50fe403a1cde
Cameron McBride <cameron.mcbride@gmail.com>
2013-02-01 05:38:41 -0500

    fixed minor alloc/free missing pair before exit

--- check_fopen.c
+++ check_fopen.c
@@ -16,23 +16,24 @@ check_fopen( const char *path, const char *mode )
 
     if( NULL == fp ) {
         char *msg;
-
         size_t len;
 
         len = strlen( path ) + strlen( mode ) + 40;
-
         msg = malloc( len * sizeof( char ) );
 
         if( NULL == msg ) {
             fprintf( stderr, "ERROR: cannot allocate a string of %zd elements\n", len );
             exit( EXIT_FAILURE );
         }
+
         snprintf( msg, len, "could not open '%s' (mode %s)", path, mode );
         if( 0 == errno ) {
             fprintf( stderr, "ERROR: %s\n", msg );
         } else {
             perror( msg );
         }
+
+        free( msg );
         exit( EXIT_FAILURE );
     }
 
